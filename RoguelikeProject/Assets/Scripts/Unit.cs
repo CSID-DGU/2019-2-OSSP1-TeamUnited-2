ed28d01a40f9a2ab11cfe0270164f90c150cd9a3
Curaria <mscu3364@gmail.com>
2019-10-09 22:00:41 +0900

    Add some comments

--- RoguelikeProject/Assets/Scripts/Unit.cs
+++ RoguelikeProject/Assets/Scripts/Unit.cs
@@ -12,20 +12,22 @@ public class Unit : MonoBehaviour
 
     protected void Move(Vector2 direction)
     {
-        //TODO:: 최대속도 구현 해야함, 현재 무한가속
-        //호출시 단위속도만큼 이동합니다. Update등의 지속호출과 연계하여야 제대로 된 움직임이 나옵니다.
-        direction.Normalize(); //방향을 단위벡터화
+        // TODO :: 최대속도 구현 해야함, 현재 무한가속
+        // 호출시 단위속도만큼 이동합니다. Update등의 지속호출과 연계하여야 제대로 된 움직임이 나옵니다.
+        direction.Normalize();
         GetComponent<Rigidbody2D>().AddForce(direction * acceleration, ForceMode2D.Impulse);
     }
 
     public void GetStrike (Strike strike)
+    // 모든 유닛의 모든 피격은 이 메서드를 사용해야만 합니다. 
+    // 직접적인 HP, transform 등의 조작은 나중에 큰 문제를 야기할 수 있습니다.
     {
-        //호출하는 순간 강한 힘으로 객체를 밀어버립니다.
-        Vector2 pushDirection = transform.position - strike.attackPosition; //(공격을 받아) 밀려날 방향
+        // 호출하는 순간 강한 힘으로 객체를 밀어버립니다.
+        Vector2 pushDirection = transform.position - strike.attackPosition;
         pushDirection.Normalize(); 
         GetComponent<Rigidbody2D>().AddForce(pushDirection * strike.attackType.force, ForceMode2D.Impulse);
 
-        //데미지가 있다면 데미지도 받습니다.
+        // 데미지가 있다면 데미지도 받습니다.
         currentHP -= strike.attackType.damage;
     }
 
@@ -33,8 +35,8 @@ public class Unit : MonoBehaviour
     {
         if (currentHP <= 0)
         {
-            //일단은 HP가 0이 되면 오브젝트를 파괴하도록 했습니다.
-            //플레이어 등의 경우 단순 오브젝트 파괴는 문제가 될 수 있으므로 각 유닛별 다른 처리가 필요할 것입니다.
+            // 일단은 HP가 0이 되면 오브젝트를 파괴하도록 했습니다.
+            // 플레이어 등의 경우 단순 오브젝트 파괴는 문제가 될 수 있으므로 각 유닛별 다른 처리가 필요할 것입니다.
             Destroy(gameObject);
         }
     }
