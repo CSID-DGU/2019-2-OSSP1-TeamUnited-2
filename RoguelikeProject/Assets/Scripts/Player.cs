94e7e6657fab039cf7a3f0b50a4388e7bcf061e2
Curaria <mscu3364@gmail.com>
2019-10-09 22:11:45 +0900

    Add comment for unit accelration todo (unstable capture)

--- RoguelikeProject/Assets/Scripts/Player.cs
+++ RoguelikeProject/Assets/Scripts/Player.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Player : Unit
 {
-  protected double invincible;
-  public Wieldable mainHand;
+    protected double invincible;
+    public Wieldable mainHand;
 
-  
+    void Update()
+    {
+        //방향키를 입력받아 Move메서드를 호출해 이동을 처리합니다.
+        int horizontal = Math.Round((Input.GetAxisRaw("Horizontal")));
+        int vertical = Math.Round((Input.GetAxisRaw("Vertical")));
+        Vector2 keyboardDirection = new Vector2(horizontal, vertical);
+        keyboardDirection.Normalize();
+        Move(keyboardDirection);
 
+        Vector2 mouseDirection = 
+
+    }
 }
