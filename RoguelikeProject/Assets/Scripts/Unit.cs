de572cee352e8328b04da7817fa010d3caac3a47
Curaria <mscu3364@gmail.com>
2019-10-09 21:40:11 +0900

    Rough implement of unit move method

--- RoguelikeProject/Assets/Scripts/Unit.cs
+++ RoguelikeProject/Assets/Scripts/Unit.cs
@@ -10,4 +10,10 @@ public class Unit : MonoBehaviour
     public double acceleration;
     public Vector2 face;
 
+    protected void Move(Vector2 direction)
+    {
+        //TODO:: 최대속도 구현 해야함, 현재 무한가속
+        direction.Normalize(); //방향을 크기가 1인 단위벡터로 정규화
+        GetComponent<Rigidbody2D>().AddForce(direction * acceleration, ForceMode2D.Impulse);
+    }
 }
