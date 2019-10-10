993f4ba5bcf9272f5f2fc6d5e9213cf0efe5ed15
Curaria <mscu3364@gmail.com>
2019-10-10 17:42:05 +0900

    minor fix

--- RoguelikeProject/Assets/Scripts/NewCodes/Strike.cs
+++ RoguelikeProject/Assets/Scripts/NewCodes/Strike.cs
@@ -8,7 +8,7 @@ public class Strike : MonoBehaviour
     public AttackTypeBase attackType;
     public GameObject attacker;
 
-    Strike(Vector2 attackPosition, double force, int damage, GameObject attacker = null)
+    Strike(Vector2 attackPosition, int damage, double force, GameObject attacker = null)
     {
         this.attackPosition = attackPosition;
         this.attackType = new AttackTypeBase(damage, force);
