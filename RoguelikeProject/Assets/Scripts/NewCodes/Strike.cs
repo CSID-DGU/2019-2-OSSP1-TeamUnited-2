f3252df7364802210a3f59c97e6fd5cfc7846b71
Curaria <mscu3364@gmail.com>
2019-10-10 17:25:11 +0900

    Rough implements of AreaStrike class, and add some TODOs

--- RoguelikeProject/Assets/Scripts/NewCodes/Strike.cs
+++ RoguelikeProject/Assets/Scripts/NewCodes/Strike.cs
@@ -7,4 +7,6 @@ public class Strike : MonoBehaviour
     public Vector2 attackPosition;
     public AttackTypeBase attackType;
     public GameObject attacker;
+
+    // TODO : 곱연산자의 오버라이딩 필요, 곱셈으로 AttackTypeBase의 force와 damage를 비율적으로 조작
 }
