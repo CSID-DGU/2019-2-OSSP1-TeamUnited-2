1e25ffe502c17e4db32a8948a62d7afd5c9e4a25
Curaria <mscu3364@gmail.com>
2019-10-11 20:08:00 +0900

    bug fix

--- RoguelikeProject/Assets/Scripts/ReStructure/ProjectileOnHit.cs
+++ RoguelikeProject/Assets/Scripts/ReStructure/ProjectileOnHit.cs
@@ -9,15 +9,17 @@ public class ProjectileOnHit : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        // 맞은 대상에게 strike 객체를 전달하여 데미지를 입힙니다.
         Strike strike = new Strike(singleAttack, transform.position, gameObject);
         if (col.gameObject.GetComponent<Unit>())
         {
             col.gameObject.GetComponent<Unit>().GetStrike(strike);
         }
 
-        Instantiate(areaStrikeGenerator, transform.position, Quaternion.identity) as GameObject;
-        areaStrikeGenerator.transform.SetParent(gameObject.transform);
-        areaStrikeGenerator.GetComponent<AreaStrike>().Activate();
+        // 범위 공격을 하는 객체를 생성하여, 작동합니다.
+        GameObject instance = Instantiate(areaStrikeGenerator, transform.position, Quaternion.identity) as GameObject;
+        instance.transform.SetParent(gameObject.transform);
+        instance.GetComponent<AreaStrike>().Activate();
 
         Destroy(gameObject);
     }
