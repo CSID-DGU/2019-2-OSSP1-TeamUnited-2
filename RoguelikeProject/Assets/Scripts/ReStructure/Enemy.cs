971863c4d132183ffb2f26781ee67788be5ea85c
sj602 <tjswls602@gmail.com>
2019-10-12 23:18:13 +0900

    add enemy class

---
+++ RoguelikeProject/Assets/Scripts/ReStructure/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Unit
+{
+    public Wieldable weapon;
+    protected GameObject target;
+    public int att;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
