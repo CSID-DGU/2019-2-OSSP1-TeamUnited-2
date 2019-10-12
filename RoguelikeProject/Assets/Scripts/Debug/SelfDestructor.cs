8fd38182284542bb73e084ea4160934be976274c
Curaria <mscu3364@gmail.com>
2019-10-13 04:50:31 +0900

    Fix missing variable of Strike class' constructor && add comment for self destructor

--- RoguelikeProject/Assets/Scripts/Debug/SelfDestructor.cs
+++ RoguelikeProject/Assets/Scripts/Debug/SelfDestructor.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelfDestructor : MonoBehaviour
+// 붙은 오브젝트를 일정시간후 파괴시키는 간이 소멸자입니다.
 {
-   
     IEnumerator SelfDestruct()
     {
         yield return new WaitForSeconds(5);
