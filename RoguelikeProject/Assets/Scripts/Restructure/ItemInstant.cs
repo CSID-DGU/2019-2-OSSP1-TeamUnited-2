ad45d15dbd908d141b6f14e93731a3b1d7dbf007
Curaria <mscu3364@gmail.com>
2019-10-13 01:39:49 +0900

    Add interface for classes

---
+++ RoguelikeProject/Assets/Scripts/Restructure/ItemInstant.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInstant : MonoBehaviour
+{
+    public int HPMod;
+
+    protected override OnTriggerStay2D(Collider2D col)
+    {
+        if (col.GetComponent<Player>())
+        {
+            
+        }
+    }
+}
---
+++ RoguelikeProject/Assets/Scripts/Restructure/ItemWieldable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemWieldable : MonoBehaviour
+{
+    public Wieldable weapon;
+
+    protected override OnTriggerStay2D(Collider2D col)
+    {
+        if (col.GetComponent<Player>())
+        {
+            
+        }
+    }
+}
