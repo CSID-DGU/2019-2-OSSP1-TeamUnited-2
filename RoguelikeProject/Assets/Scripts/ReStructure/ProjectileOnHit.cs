ec7aa1bead428e76f6172e25e203c1ae5a4fc8bc
Curaria <mscu3364@gmail.com>
2019-10-13 01:11:34 +0900

    Projectiles now ignore coillision from shooter

--- RoguelikeProject/Assets/Scripts/ReStructure/ProjectileOnHit.cs
+++ RoguelikeProject/Assets/Scripts/ReStructure/ProjectileOnHit.cs
@@ -13,7 +13,7 @@ public class ProjectileOnHit : MonoBehaviour
     {
         this.attacket = attacker;
     }
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
 
         // 범위공격 객체는 생성과 동시에 작동합니다.
@@ -21,12 +21,16 @@ public class ProjectileOnHit : MonoBehaviour
         // areaStrikeGenerator.AddComponent<AreaStrike>();
         // areaStrikeGenerator.GetComponent<AreaStrike>().SetStatus(damage, force, radius);
 
-        // 맞은 대상에게 strike 객체를 전달하여 데미지를 입힙니다.
-        Strike strike = new Strike(damage, force, transform.position);
-        if (col.gameObject.GetComponent<Unit>())
+        // 공격자는 투사체에 면역입니다. (일단은)
+        if (col == attacker)
+            return;
+
+        // 대상이 유닛인 경우 맞은 대상에게 strike 객체를 전달하여 데미지를 입힙니다.
+        GameObject targetUnit = col.gameObject.GetComponent<Unit>();
+        if (targetUnit)
         {
-            Debug.Log("Someone Hit");
-            col.gameObject.GetComponent<Unit>().GetStrike(strike);
+            Strike strike = new Strike(damage, force, transform.position);
+            targetUnit.GetStrike(strike);
         }
 
         // 범위 공격을 하는 객체를 생성하여, 작동합니다.
