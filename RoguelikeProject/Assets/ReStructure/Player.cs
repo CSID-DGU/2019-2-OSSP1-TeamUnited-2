781188526b69e02fcad71981791b8bb23ae27177
Curaria <mscu3364@gmail.com>
2019-10-16 11:34:30 +0900

    Rearrange damage processing codes & comments to improve readability

--- RoguelikeProject/Assets/ReStructure/Player.cs
+++ RoguelikeProject/Assets/ReStructure/Player.cs
@@ -22,7 +22,7 @@ public class Player : Unit
     // 플레이어의 HP조작은 반드시 이 메서드를 통해서만 이루어져야 합니다.
     {
         // 실제로 받게 되는 데미지를 추적합니다.
-        int actualDamage = damage;
+        int actualDamage;
 
         // 회복인 경우 (데미지가 음수)
         if (damage < 0)
@@ -36,8 +36,13 @@ public class Player : Unit
         {
             actualDamage = 0;
         }
+        // 그 외에는 데미지를 그대로 받습니다 (일단은)
+        else
+        {
+            actualDamage = damage;
+        }
 
-        // 실제 피해 연산 (회복일 수 있습니다)
+        // 실제 연산된 피해 결과를 HP에 적용합니다. (회복일 수 있습니다)
         currentHP -= actualDamage;
 
         return actualDamage;
