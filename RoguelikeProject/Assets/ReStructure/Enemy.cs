33c73bb04adce48545cd2a4020d01e06e3fe9130
xogur6889 <chlxogur_@naver.com>
2019-10-17 11:37:31 +0900

    애니메이션 추가. 크기늘림.

--- RoguelikeProject/Assets/ReStructure/Enemy.cs
+++ RoguelikeProject/Assets/ReStructure/Enemy.cs
@@ -8,14 +8,28 @@ public class Enemy : Unit
     protected GameObject target;
     public int att;
     public double force;
+    Animator anim;
+    float once; // 애니메이션 트리거 한번만 작동하게..
 
+    new void Start()
+    {
+        currentHP = HP;
+        anim = GetComponent<Animator>();
+    }
     protected new void Update()
     {
         base.Update();
+        once -= Time.deltaTime;
     }
     void OnCollisionStay2D(Collision2D coll)
     {
         if (coll.gameObject.name == "Player")
+        {
             coll.gameObject.GetComponent<Player>().GetStrike(new Strike(att, force, transform.position));
+
+            if (once < 0) // 트리거 한번만 실행되게..
+                anim.SetTrigger("New Trigger");
+            once = 1f;
+        }
     }
 }
