d8d8ccdca522b432542d98d5f7885776aee7b50d
Curaria <mscu3364@gmail.com>
2019-10-31 10:44:38 +0900

    Add some comments

--- RoguelikeProject/Assets/Scripts/ReStructure/ProjectileManager.cs
+++ RoguelikeProject/Assets/Scripts/ReStructure/ProjectileManager.cs
@@ -7,15 +7,18 @@ public class ProjectileManager
 {
     public ProjectileAttribute attribute;
     public GameObject shape; // Sprite, RigidBody2D, Collider로 구성된 형체 오브젝트
-    protected GameObject entity = null;
+    protected GameObject entity = null; // 실제 투사체를 생성하기 위한 복제본. 발사가능한 투사체와 동일한 객체여야 합니다.
     public GameObject Entity 
     {
+        // 호출과 동시에 객체를 생성합니다.
         get
         {
+            // 객체가 생성된 적이 있다면 그대로 반환합니다.
             if (entity != null) 
             {
                 return entity;
             }
+            // 객체를 그냥 호출하였을 경우 생성해야합니다. Shape의 물리적 속성에 투사체의 속성(ProjectileOnHit)을 더합니다.
             else
             {
                 entity = shape;
@@ -23,6 +26,7 @@ public class ProjectileManager
                 return entity;
             }
         }
+        // 직접 객체를 불러와 만들 수도 있습니다.
         set { entity = value; }
     }
 }
