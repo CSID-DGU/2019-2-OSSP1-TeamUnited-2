029ea49cd6321428cc379fb98ba5d37e89c28b89
sj602 <tjswls602@gmail.com>
2019-11-03 16:02:47 +0900

    test

--- RoguelikeProject/Assets/Scripts/ReStructure/Enemy.cs
+++ RoguelikeProject/Assets/Scripts/ReStructure/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy : Unit
 {
     public Wieldable weapon;
-    protected GameObject target;
+    public GameObject target;
     public int meleeDamage;
     public double force;
 
@@ -22,36 +22,16 @@ public class Enemy : Unit
         // 애니메이터가 있다면 설정해줍니다.
         if (GetComponent<Animator>())
             animator = GetComponent<Animator>();
-
-        InvokeRepeating("findPlayer", 0, 1);
     }
     protected new void Update()
     {
         base.Update();
-
+        Vector2 direction = target.transform.position - transform.position;
+        Move(direction);
     }
     void OnCollisionStay2D(Collision2D coll)
     {
         if (coll.gameObject.name == "Player")
             coll.gameObject.GetComponent<Player>().GetStrike(new Strike(meleeDamage, force, transform.position));
-    }
-    void findPlayer()
-    {
-        if (target.GetComponent<Player>())
-            return;
-
-        if (this.gameObject.name == "Enemy")
-        {
-            Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, 40.0f);
-            foreach (Collider2D col in cols)
-            {
-                Debug.Log(col);
-                if (col.gameObject.GetComponent<Player>())
-                {
-                    Vector2 direction = col.gameObject.GetComponent<Player>().transform.position - transform.position;
-                    Move(direction);
-                }
-            }
-        }
     }
 }
