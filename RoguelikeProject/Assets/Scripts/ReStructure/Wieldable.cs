ace31a056b23cf8e6ec5190dcf10b5271ad41d02
Curaria <mscu3364@gmail.com>
2019-11-03 19:12:13 +0900

    Put ProjectileManager class into Wieldable abstract class (because it can't be used outside of Wieldable class, due to structural reason)

--- RoguelikeProject/Assets/Scripts/ReStructure/Wieldable.cs
+++ RoguelikeProject/Assets/Scripts/ReStructure/Wieldable.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public abstract class Wieldable : MonoBehaviour
 {
+    [System.Serializable]
+    public class ProjectileManager
+    {
+        public ProjectileAttribute attribute;
+        public GameObject shape; // Sprite, Collider, RigidBody로 구성된 형체 오브젝트
+        protected GameObject entity = null; // 실제 투사체를 생성하기 위한 샘플. 발사가능한 투사체와 동일한 속성이어야 합니다.
+        public GameObject Entity 
+        {
+            get { return entity; }
+            set { entity = value; }
+        }
+    }
     protected GameObject owner;
     public GameObject Owner
     {
