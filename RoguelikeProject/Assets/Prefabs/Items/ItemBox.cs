eb947d2966a9cd3452fa2c260d13af06daacee6e
taehyuk choi <chlxogur_@naver.com>
2019-12-16 20:58:00 +0900

    ItemBoxImgChange

--- RoguelikeProject/Assets/Prefabs/Items/ItemBox.cs
+++ RoguelikeProject/Assets/Prefabs/Items/ItemBox.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class ItemBox : Unit
 {
+    private enum SCALEDIRECTION
+    {
+        UP = 0,
+        DOWN = 1,
+    }
+    private SCALEDIRECTION ScaleDirection;
+    private float ScaleXY = 1.0f;
+
+    private float StartingScale = 0.0f;
     public GameObject bombItem;
     protected override void Start()
     {
         currentHP = HP;
+        StartingScale = transform.localScale.x;
     }
     protected new void Update()
     {
         base.Update();
+        // change the scale factor the object based on the current state
+        if (ScaleDirection == SCALEDIRECTION.UP)
+        {
+            ScaleXY += 0.5f * Time.deltaTime;
+        }
+        else if (ScaleDirection == SCALEDIRECTION.DOWN)
+        {
+            ScaleXY -= 0.5f * Time.deltaTime;
+        }
 
+        // limit the scale in both directions
+        if (ScaleXY > 1.15f)
+        {
+            ScaleDirection = SCALEDIRECTION.DOWN;
+            ScaleXY = 1.15f;
+        }
+
+        if (ScaleXY < 0.85f)
+        {
+            ScaleDirection = SCALEDIRECTION.UP;
+            ScaleXY = 0.85f;
+        }
+
+        // apply the scale factor
+        transform.localScale = new Vector3(StartingScale * ScaleXY, StartingScale * ScaleXY, transform.localScale.z);
     }
     protected override void SelfDestruction()
     {
